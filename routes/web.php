405a8925c38f2d0115742ebe660e85265274941d
AlexJmzc <jjimenez2439@uta.edu.ec>
2022-09-08 16:10:40 -0500

    Sidenav y header

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {   
-    return view('detalleequipos');
+    return view('livewire.pantalla-equipo');
 });
 
 Route::get('/principal', function () {
